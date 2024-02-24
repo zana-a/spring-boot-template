429f8d71b7c0481a9ae9543e2e82e4303e5aa98a
zana-a <zana200@icloud.com>
2024-02-24 20:23:32 +0000

    replace square-intellij-java-style-guide.xml with intellij-java-google-style.xml.xml

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,47 +1,47 @@
 plugins {
-  id("java")
-  id("org.springframework.boot") version "3.2.3"
-  id("io.spring.dependency-management") version "1.1.4"
+    id("java")
+    id("org.springframework.boot") version "3.2.3"
+    id("io.spring.dependency-management") version "1.1.4"
 }
 
 group = "org.example"
 version = "0.1.0"
 
 repositories {
-  mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-  compileOnly("org.projectlombok:lombok")
-  annotationProcessor("org.projectlombok:lombok")
-  implementation("com.google.guava:guava:33.0.0-jre")
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+    implementation("com.google.guava:guava:33.0.0-jre")
 
-  implementation("org.springframework.boot:spring-boot-starter-web")
-  implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
-  testImplementation(platform("org.junit:junit-bom:5.10.2"))
-  testImplementation("org.junit.jupiter:junit-jupiter")
-  runtimeOnly("org.postgresql:postgresql")
-  testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-  testImplementation("org.mockito:mockito-core:2.1.0")
-  testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation(platform("org.junit:junit-bom:5.10.2"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    runtimeOnly("org.postgresql:postgresql")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.mockito:mockito-core:2.1.0")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 configurations {
-  compileOnly {
-    extendsFrom(configurations.annotationProcessor.get())
-  }
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
 }
 
 java {
-  sourceCompatibility = JavaVersion.VERSION_21
-  targetCompatibility = JavaVersion.VERSION_21
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
 }
 
 tasks.withType<Test> {
-  useJUnitPlatform()
+    useJUnitPlatform()
 }
 
 springBoot {
-  mainClass = "org.example.SpringBootTemplateApplication"
+    mainClass = "org.example.SpringBootTemplateApplication"
 }
